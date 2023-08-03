4ffd1e7ce899331a232a75efd0c05eaf22704584
Arisna <agus.aris83@gmail.com>
2023-08-04 01:36:05 +0800

    new: add minimax algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,92 @@
 #![allow(unused)]
 
+use std::collections::HashMap;
+use std::fs::OpenOptions;
 use std::io;
+use std::io::Write;
+use std::process::Command;
 use std::{fmt::format, usize};
+
+use tabled::{
+    builder::Builder,
+    settings::{object::Rows, Alignment, Modify, Style},
+};
 
 fn main() {
     const TOTAL_ROWS: usize = 3;
     const TOTAL_COLUMNS: usize = 3;
     const MAX_FILL: usize = TOTAL_ROWS * TOTAL_COLUMNS;
     let mut board = create_board(TOTAL_ROWS, TOTAL_COLUMNS);
+
     let mut game_end = false;
-    let p1_char = ask_player_char();
-    let p2_char = (if p1_char == 'X' { 'O' } else { 'X' });
+    clearscreen();
+    println!("[*] WELCOME TO TICTACTOE GAME [*]");
+    let human_char = ask_player_char();
+    let ai_char = (if human_char == 'X' { 'O' } else { 'X' });
     let mut filled_box_count = 0;
     let mut winner = ' ';
+    let mut ai_last_move = 0;
+
     while filled_box_count < MAX_FILL {
-        print_board(board.clone());
-        loop {
-            let p1_move_num = ask_player_move();
-            let p1_move = move_num_to_array(p1_move_num, TOTAL_ROWS);
-            if board[p1_move[0]][p1_move[1]] == ' ' {
-                fill_box(&mut board, p1_move[0], p1_move[1], p1_char);
-                filled_box_count += 1;
+        if ai_char == 'X' {
+            clearscreen();
+            let ai_move = ai_best_move(&mut board, ai_char, human_char);
+            fill_box(&mut board, ai_move[0], ai_move[1], ai_char);
+            filled_box_count += 1;
+            print_board(board.clone());
+            println!(
+                "[+] AI move : X -> {}",
+                move_array_to_num(ai_move, TOTAL_ROWS)
+            );
+            if is_win(board.clone(), ai_char) {
+                winner = ai_char;
                 break;
-            } else {
-                println!("{} is already filled!", p1_move_num)
             }
-        }
-        if is_win(board.clone(), p1_char) {
-            winner = p1_char;
-            break;
-        }
-        let p2_move = ai_move(board.clone());
-        fill_box(&mut board, p2_move[0], p2_move[1], p2_char);
-        filled_box_count += 1;
-        if is_win(board.clone(), p2_char) {
-            winner = p2_char;
-            break;
+            let human_move = ask_player_move(board.clone(), human_char);
+            fill_box(&mut board, human_move[0], human_move[1], human_char);
+            filled_box_count += 1;
+            if is_win(board.clone(), human_char) {
+                winner = human_char;
+                break;
+            }
+        } else {
+            clearscreen();
+            print_board(board.clone());
+            if ai_last_move == 0 {
+                println!("[*] AI is waiting your move...");
+            } else {
+                println!("[+] AI move: O -> {}", ai_last_move);
+            }
+            let human_move = ask_player_move(board.clone(), human_char);
+            fill_box(&mut board, human_move[0], human_move[1], human_char);
+            filled_box_count += 1;
+            if is_win(board.clone(), human_char) {
+                winner = human_char;
+                break;
+            }
+            let ai_move = ai_best_move(&mut board, ai_char, human_char);
+            fill_box(&mut board, ai_move[0], ai_move[1], ai_char);
+            filled_box_count += 1;
+            if is_win(board.clone(), ai_char) {
+                winner = ai_char;
+                break;
+            }
+            ai_last_move = move_array_to_num(ai_move, TOTAL_ROWS);
         }
     }
-    if winner == p1_char {
-        println!("YOU WIN!");
-    } else if winner == p2_char {
-        println!("YOU LOSE!");
+    clearscreen();
+    if winner == human_char {
+        println!("[*] YOU ({}) WIN [*]", human_char);
+    } else if winner == ai_char {
+        println!("[*] YOU ({}) LOSE! [*]", human_char);
     } else {
-        println!("DRAW!");
+        println!("[*] DRAW! [*]");
     }
     print_board(board.clone());
+}
+
+fn clearscreen() {
+    print!("\x1B[2J\x1B[1;1H");
 }
 
 fn move_array_to_num(move_arr: [usize; 2], board_rows: usize) -> usize {
@@ -58,37 +99,152 @@ fn move_num_to_array(num: usize, board_rows: usize) -> [usize; 2] {
     return [i, j];
 }
 
-fn ask_player_move() -> usize {
-    println!("Your move (1-9): ");
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-    let player_move: usize = input.trim().parse().expect("Please enter a number");
-    return player_move;
+fn ask_player_move(board: Vec<Vec<char>>, human_char: char) -> [usize; 2] {
+    loop {
+        println!("[+] Your move {} -> (1-9)?: ", human_char);
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let player_move: usize = input.trim().parse().expect("Please enter a number");
+        let player_move_array = move_num_to_array(player_move, board.len());
+        if board[player_move_array[0]][player_move_array[1]] != ' ' {
+            println!("[!] Invalid: {} already filled", player_move)
+        } else {
+            return player_move_array;
+        }
+    }
 }
 
-fn ai_move(board: Vec<Vec<char>>) -> [usize; 2] {
+fn write_ai_log(data: String) {
+    // Open the file in append mode
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("ai.log")
+        .expect("Failed to open file");
+
+    // Write the data to the file
+    if let Err(err) = file.write_all(data.as_bytes()) {
+        eprintln!("Error writing to file: {}", err);
+    }
+}
+
+fn minimax(
+    board: &mut Vec<Vec<char>>,
+    is_maximizing: bool,
+    depth: isize,
+    ai_char: char,
+    human_char: char,
+) -> isize {
+    let result = check_winner(board.clone());
+    // print_board(board.to_vec());
+    // println!("Depth: {} ", depth);
+    if result != ' ' {
+        if result == 'D' {
+            return 0;
+        } else if result == human_char {
+            return -100;
+        } else {
+            return 100;
+        }
+    }
+
     let x_length = board.len();
     let y_length = board[0].len();
+
+    if is_maximizing {
+        let mut best_score = -100;
+        for i in 0..x_length {
+            for j in 0..y_length {
+                if board[i][j] == ' ' {
+                    board[i][j] = ai_char;
+                    let score = minimax(board, false, depth + 1, ai_char, human_char);
+                    board[i][j] = ' ';
+                    if score > best_score {
+                        best_score = score;
+                    }
+                }
+            }
+        }
+        return best_score - depth;
+    } else {
+        let mut best_score = 100;
+        for i in 0..x_length {
+            for j in 0..y_length {
+                if board[i][j] == ' ' {
+                    board[i][j] = human_char;
+                    let score = minimax(board, true, depth + 1, ai_char, human_char);
+                    board[i][j] = ' ';
+                    if score < best_score {
+                        best_score = score;
+                    }
+                }
+            }
+        }
+        return best_score - depth;
+    }
+}
+
+fn ai_best_move(board: &mut Vec<Vec<char>>, ai_char: char, human_char: char) -> [usize; 2] {
+    let x_length = board.len();
+    let y_length = board[0].len();
+    let mut best_score = -100;
+    let mut best_move: [usize; 2] = Default::default();
+    write_ai_log("AI's Possible moves:\n".to_string());
     for i in 0..x_length {
         for j in 0..y_length {
             if board[i][j] == ' ' {
-                return [i, j];
+                board[i][j] = ai_char;
+                let score = minimax(board, false, 1, ai_char, human_char);
+                board[i][j] = ' ';
+                let move_num = move_array_to_num([i, j], x_length);
+                write_ai_log(format!("- {} -> Score: {}\n", move_num, score));
+                if score > best_score {
+                    best_score = score;
+                    best_move = [i, j];
+                }
             }
         }
     }
-    return [0, 0];
+    write_ai_log(format!(
+        "AI's Best move: {}\n\n",
+        move_array_to_num(best_move, x_length)
+    ));
+    return best_move;
 }
 
 fn ask_player_char() -> char {
-    println!("Chose X or O :");
+    println!("[*] First/second (X/O)?:");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
     let character: char = input.trim().chars().next().expect("No input provided.");
     return character.to_ascii_uppercase();
+}
+
+fn check_winner(board: Vec<Vec<char>>) -> char {
+    if is_win(board.clone(), 'X') {
+        return 'X';
+    } else if is_win(board.clone(), 'O') {
+        return 'O';
+    }
+
+    let mut filled_count = 0;
+    let x_length = board.len();
+    let y_length = board[0].len();
+    for i in 0..x_length {
+        for j in 0..y_length {
+            if board[i][j] != ' ' {
+                filled_count += 1;
+            }
+        }
+    }
+    if filled_count == x_length * y_length {
+        return 'D';
+    }
+    return ' ';
 }
 
 fn is_win(board: Vec<Vec<char>>, player_char: char) -> bool {
@@ -128,28 +284,22 @@ fn print_board(board: Vec<Vec<char>>) {
     let x_length = board.len();
     let y_length = board[0].len();
 
-    // print row index guide top
-    let mut row_guide = String::new();
-    row_guide.push(' '); // for column guide
+    let mut builder = Builder::default();
     for i in 0..x_length {
-        let column_str = format!(" {}", i + 1);
-        row_guide.push_str(&column_str)
-    }
-    println!("{}", row_guide);
-    // print each board row
-    for i in 0..x_length {
-        let mut row_str = String::new();
-        row_str.push_str(&(i + 1).to_string());
+        let mut row: Vec<char> = Vec::new();
         for j in 0..y_length {
-            let column_str = format!(" {}", board[i][j]);
-            row_str.push_str(&column_str)
+            if board[i][j] == ' ' {
+                let box_num = (i * x_length + j + 1);
+                let box_num_char = (b'0' + box_num as u8) as char;
+                row.push(box_num_char);
+            } else {
+                row.push(board[i][j]);
+            }
         }
-        row_str.push_str(&format!(" {}", i + 1));
-        println!("{}", row_str);
+        builder.push_record(row);
     }
-    // print row index guide bottom
-    println!("{}", row_guide);
-    println!();
+    let table = builder.build().with(Style::modern()).to_string();
+    println!("{}", table);
 }
 
 // create tic tac toe board
